127d3fd6b84259f0c82991d9a1de46bb16a84f59
Cmanchik <aksenova1648@gmail.com>
2024-01-31 00:52:19 +0400

    Шаблон для создание задания

---
+++ Assets/Scripts/QuestScriptableObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts
+{
+    [CreateAssetMenu(fileName = "NewQuest", menuName = "ScriptableObjects/Quest", order = 1)]
+    internal class QuestScriptableObject : ScriptableObject
+    {
+        [SerializeField]
+        private String name;
+
+        [SerializeField]
+        private String description;
+
+        [SerializeField]
+        private Sprite imageDescription;
+
+        [SerializeField]
+        private String questGiver;
+
+        [SerializeField]
+        private String rightAnswer;
+    }
+}

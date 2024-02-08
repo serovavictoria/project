1fd0c6dd5a7e653ae7a0a97cdff2141d282ba13f
Cmanchik <aksenova1648@gmail.com>
2024-02-08 04:28:48 +0400

    Update StartGame.cs

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -12,6 +12,6 @@ public class StartGame : MonoBehaviour
     {
         Debug.Log(DateTime.Now);
         StartGameTime.ticks = DateTime.Now.Ticks;
-        PlayerPrefs.SetFloat("assessment", 4);
+        PlayerPrefs.SetFloat("assessment", 0);
     }
 }
